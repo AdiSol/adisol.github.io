0daf94f7a79103e4289b1841498458daa56e3176
Adrienne Soliven <adriennesoliven@yahoo.com>
2022-03-24 02:27:34 +0800

    fixed auto scrolls and added it in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,42 +1,54 @@
 import './Footer.css'
 
-const Footer = () => (
-  <footer className='footer'>
-    <a
-      href='/#/#projects/'
-      className='link footer__link'
-    >
-      all
-    </a>
+const Footer = () => {
 
-    <a
-      href='/#/AnimoSched/'
-      className='link footer__link'
-    >
-      AnimoSched
-    </a>
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  }
+   return(
+      <footer className='footer' onClick={scrollToTop}>
+        <a
+          href='/#/#projects/'
+          className='link footer__link'
+        >
+          all
+        </a>
 
-    <a
-      href='/#/CoDo/'
-      className='link footer__link'
-    >
-      CoDO
-    </a>
+        <a
+          href='/#/AnimoSched/'
+          className='link footer__link'
+        >
+          AnimoSched
+        </a>
 
-    <a
-      href='/#/AkadsPH/'
-      className='link footer__link'
-    >
-      AkadsPH
-    </a>
+        <a
+          href='/#/CoDo/'
+          className='link footer__link'
+        >
+          CoDO
+        </a>
 
-    <a
-      href='/#/TranspoDesire/'
-      className='link footer__link'
-    >
-      TranspoDesire
-    </a>
-  </footer>
-)
+        <a
+          href='/#/AkadsPH/'
+          className='link footer__link'
+        >
+          AkadsPH
+        </a>
+
+        <a
+          href='/#/TranspoDesire/'
+          className='link footer__link'
+        >
+          TranspoDesire
+        </a>
+      </footer>
+  ) 
+
+}
 
 export default Footer
